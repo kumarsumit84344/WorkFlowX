d289566a06032ffa799b430b1cf4106a8db0bf97
Sumit Kumar <70761681+kumarsumit84344@users.noreply.github.com>
2026-02-10 09:08:27 +0000

    Firebase setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, PLATFORM_ID, inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { AuthService } from './core/auth/auth.guard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  template: `
+    <h1>Firebase Auth Test</h1>
+
+    @if (isBrowser) {
+      <button (click)="register()">Register</button>
+      <button (click)="login()">Login</button>
+    }
+  `
 })
 export class App {
-  protected readonly title = signal('WorkFlowx');
+  private platformId = inject(PLATFORM_ID);
+  isBrowser = isPlatformBrowser(this.platformId);
+  
+  constructor(private auth: AuthService) {}
+
+  register() {
+    this.auth.register('test@test.com', '123456')
+      .then(res => console.log('Registered:', res))
+      .catch(err => console.error(err));
+  }
+
+  login() {
+    this.auth.login('test@test.com', '123456')
+      .then(res => console.log('Logged in:', res))
+      .catch(err => console.error(err));
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,9 @@
-export const environment = {
-  production: false
+export const firebaseConfig = {
+  apiKey: "AIzaSyBuvBaVN31RdQQeH-Box2V2zi6hNzxZvdI",
+  authDomain: "workflowx-9370b.firebaseapp.com",
+  projectId: "workflowx-9370b",
+  storageBucket: "workflowx-9370b.appspot.com",
+  messagingSenderId: "1014039443519",
+  appId: "1:1014039443519:web:8284d37d932a73d1a9a08a",
+  measurementId: "G-Y3RMBXKF48"
 };
